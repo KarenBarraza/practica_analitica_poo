16a075b35484b45ad9bb26b256d077cb8aa4d446
Karen Gisell Rodriguez Barraza <barrazak940>
2025-11-18 10:11:27 -0500

    feat: Se habilita la opcion #4 donde se calcula el area del cuadrado y la #5 que es salir del programa

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ fg=FiguraGeometrica
 
 menuActivo = True
 while menuActivo:
+    print("---------------------------------")
     print("Calculo areas figuras geometricas")
     print("1. Triangulo")
     print("2. Circulo")
@@ -21,16 +22,26 @@ while menuActivo:
         altura = float(input("Ingrese la altura del triangulo: "))
         tr=Triangulo(base,altura)
         area = tr.area()
-        print("El area es:", area)
+        print("El area del triangulo es:", area)
     elif opcion == "2":
         radio = float(input("Indique el radio del circulo:"))
         cr = Circulo(radio)
         area = cr.area()
-        print("El area es:", area)
+        print("El area del circulo es:", area)
     elif opcion == "3":
         base = float(input("Ingrese la base del rectangulo: "))
         altura = float(input("Ingrese la altura del rectangulo: "))
         rt=Rectangulo(base, altura)
         area= rt.area()
-        print("El area es:", area)   
-             
+        print("El area del rectangulo es:", area)   
+    elif opcion == "4":
+        lado = float(input("Ingrese la medida de uno de los lados: "))
+        cd=Cuadrado(lado)
+        area=cd.area()
+        print("El area del cuadrado es:", area)
+    elif opcion =="5":
+        print("Saliendo del programa")
+        break
+
+
+         
